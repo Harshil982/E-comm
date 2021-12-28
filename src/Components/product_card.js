97ab0e0ee8bdb1ae5ecd02fb45751bb01205d074
unknown <rajputharshil982002@gmail.com>
2021-12-28 21:30:47 +0530

    Files

--- src/Components/product_card.js
+++ src/Components/product_card.js
@@ -10,8 +10,18 @@ const ProductCard = () => {
 
     const handleAddToCart = (e) =>{
         console.log(e);
-        console.log(e.target.value);
+        if(e.target.parentNode.className === "add-to-cart"){
+            console.log(e.target.parentNode.value);
+        }
+        else
+        {
+            console.log(e.target.value)
+        }
+        // console.log(e.target.parentNode.value);
+        // console.log(e.target.parentNode.className);
     }
+
+    
     return (
         <>
             <div className="Products-container">
@@ -42,7 +52,42 @@ const ProductCard = () => {
                         <h3>Cream</h3>
                         <p>All organic Ingrediants</p>
                         <p>$24.99 </p>
-                        <button className="add-to-cart" value="Cream4" onClick={handleAddToCart}><FontAwesomeIcon icon={faCartPlus} /><span className="cart-text" value="Cream4">Add to Cart</span></button>
+                        <button className="add-to-cart" value="Cream4" onClick={handleAddToCart}><FontAwesomeIcon icon={faCartPlus} /><span className="cart-text" value="Cream3">Add to Cart</span></button>
+                    </Grid>
+                    <Grid className="Product-info-card" item xs={10} sm={5} md={6} lg={4}>
+                        <img src={Image} className="Product-image" alt="Product" />
+                        <h3>Cream</h3>
+                        <p>All organic Ingrediants</p>
+                        <p>$24.99 </p>
+                        <button className="add-to-cart" value="Cream4" onClick={handleAddToCart}><FontAwesomeIcon icon={faCartPlus} /><span className="cart-text" value="Cream3">Add to Cart</span></button>
+                    </Grid>
+                    <Grid className="Product-info-card" item xs={10} sm={5} md={6} lg={4}>
+                        <img src={Image} className="Product-image" alt="Product" />
+                        <h3>Cream</h3>
+                        <p>All organic Ingrediants</p>
+                        <p>$24.99 </p>
+                        <button className="add-to-cart" value="Cream4" onClick={handleAddToCart}><FontAwesomeIcon icon={faCartPlus} /><span className="cart-text" value="Cream3">Add to Cart</span></button>
+                    </Grid>
+                    <Grid className="Product-info-card" item xs={10} sm={5} md={6} lg={4}>
+                        <img src={Image} className="Product-image" alt="Product" />
+                        <h3>Cream</h3>
+                        <p>All organic Ingrediants</p>
+                        <p>$24.99 </p>
+                        <button className="add-to-cart" value="Cream4" onClick={handleAddToCart}><FontAwesomeIcon icon={faCartPlus} /><span className="cart-text" value="Cream3">Add to Cart</span></button>
+                    </Grid>
+                    <Grid className="Product-info-card" item xs={10} sm={5} md={6} lg={4}>
+                        <img src={Image} className="Product-image" alt="Product" />
+                        <h3>Cream</h3>
+                        <p>All organic Ingrediants</p>
+                        <p>$24.99 </p>
+                        <button className="add-to-cart" value="Cream4" onClick={handleAddToCart}><FontAwesomeIcon icon={faCartPlus} /><span className="cart-text" value="Cream3">Add to Cart</span></button>
+                    </Grid>
+                    <Grid className="Product-info-card" item xs={10} sm={5} md={6} lg={4}>
+                        <img src={Image} className="Product-image" alt="Product" />
+                        <h3>Cream</h3>
+                        <p>All organic Ingrediants</p>
+                        <p>$24.99 </p>
+                        <button className="add-to-cart" value="Cream4" onClick={handleAddToCart}><FontAwesomeIcon icon={faCartPlus} /><span className="cart-text" value="Cream3">Add to Cart</span></button>
                     </Grid>
                 </Grid>
             </div>
