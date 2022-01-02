63199576610441a67ea6202c60d1613b2888aa5e
unknown <rajputharshil982002@gmail.com>
2022-01-03 00:04:45 +0530

    Files

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './../Styles/Footer.css'
 
 function Footer() {
+    const [inputValue, setInputValue] = useState('')
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value)
+        console.log(inputValue);
+    }
     return (
-        <>
-            <h1>Footer</h1>  
-        </>
+        <div className="subscribe">
+            <div className="flex-cont">
+                <h1>Join the Sooth family</h1>
+                <div className="verticle-divider"></div>
+                <p>lorem ipsum dolor si gnjdn kdnfm kdndk ksnenek pwkndk</p>
+            </div>
+            <div className="flex-1">
+                <input type="text" onChange={handleInputChange} />
+                <button>Subscribe</button>
+            </div>
+        </div>
     );
 }
 
