aec55e343ec9c0b2f34024bac655bd9176e49c00
unknown <rajputharshil982002@gmail.com>
2022-01-20 23:39:34 +0530

    Files

--- src/Components/Common Components/NotFound.js
+++ src/Components/Common Components/NotFound.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './NotFound.css'
 
 function NotFound(props) {
     return (
-        <div>
-            <h1>Page Not found</h1>
-        </div>
+        <>
+            <div class="error">404</div>
+            <br /><br />
+            <span class="info">File not found</span>
+            <img src="http://images2.layoutsparks.com/1/160030/too-much-tv-static.gif" class="static" alt="hello" />
+        </>
     );
 }
 
